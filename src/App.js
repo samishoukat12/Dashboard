2f4d9450cbfef32e1e90fc8e6e916708c9f4f1ac
Sami Shoukat <62543601+muhammadsamishoukat@users.noreply.github.com>
2021-04-24 10:22:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ResponsiveDrawer from './component/navbar/navbar';
+
+
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
+// import Button from '@material-ui/core/Button';
 
 function App() {
+  // const containerP="Hy! I am sami shoukat"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
 
+      <ResponsiveDrawer />
+      
+
+      </>
+
+)
+};
 export default App;
